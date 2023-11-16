0a6ddfb01cde520ac086e2db6000b9ae63581841
ahmadsultani <dayanullahas21d@student.unhas.ac.id>
2023-11-16 13:52:13 +0800

    feat: auth types

---
+++ feature/auth/types.ts
@@ -0,0 +1,11 @@
+export type TLoginForm = {
+  email: string;
+  password: string;
+};
+
+export type TSignupForm = {
+  email: string;
+  password: string;
+  firstName: string;
+  lastName: string;
+};
