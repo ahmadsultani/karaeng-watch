126bc1e3374ffa58b34331aeeec01c73cf2bcb0a
ahmadsultani <dayanullahas21d@student.unhas.ac.id>
2023-11-16 13:39:54 +0800

    feat: reusable auth layout

---
+++ feature/auth/AuthLayout.tsx
@@ -0,0 +1,35 @@
+import Image from "next/image";
+
+import { ImageContainer } from "@/components/ImageContainer";
+
+import { AuthContainer, AuthImageContainer, AuthWrapper } from "./styles";
+import { Box } from "@mui/material";
+
+interface AuthLayoutProps {
+  children: React.ReactNode;
+}
+
+export const AuthLayout: React.FC<AuthLayoutProps> = ({ children }) => {
+  return (
+    <AuthWrapper>
+      <AuthContainer>
+        {children}
+        <AuthImageContainer>
+          <Box
+            position="absolute"
+            top="0"
+            left="0"
+            width="100%"
+            height="100%"
+            bgcolor="#00000099"
+            zIndex={1}
+          />
+          <Image src="/images/auth-bg.webp" alt="background" fill priority />
+          <ImageContainer size="240px">
+            <Image src="/logos/logo-white.svg" alt="logo" fill />
+          </ImageContainer>
+        </AuthImageContainer>
+      </AuthContainer>
+    </AuthWrapper>
+  );
+};
