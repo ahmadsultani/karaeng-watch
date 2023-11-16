602260f87bd6f093720b0adf6eba4e0188d0ca2e
ahmadsultani <dayanullahas21d@student.unhas.ac.id>
2023-11-16 13:37:55 +0800

    feat: setup MUI theme and colors

---
+++ constants/colors.tsx
@@ -0,0 +1,4 @@
+export const COLORS = {
+  lightGrey: "#F5F5F5",
+  grey: "#757575BF",
+};
---
+++ constants/theme.tsx
@@ -0,0 +1,52 @@
+import { createTheme } from "@mui/material/styles";
+
+export const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#EAC35F",
+    },
+    secondary: {
+      main: "#222222",
+    },
+    info: {
+      main: "#306BEC",
+    },
+  },
+  typography: {
+    fontFamily: "inherit",
+  },
+  components: {
+    MuiTextField: {
+      defaultProps: {
+        variant: "standard",
+      },
+      styleOverrides: {
+        root: {
+          borderColor: "secondary.main",
+          "& input": {
+            color: "secondary.main",
+            padding: "12px 8px",
+          },
+
+          "& label:not(.MuiInputLabel-shrink)": {
+            marginLeft: "8px",
+            marginTop: "8px",
+          },
+        },
+      },
+    },
+
+    MuiButton: {
+      defaultProps: {
+        variant: "contained",
+      },
+      styleOverrides: {
+        root: {
+          borderRadius: 0,
+          padding: "12px",
+          textTransform: "none",
+        },
+      },
+    },
+  },
+});
