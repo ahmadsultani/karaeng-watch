84e473fbc2130405b4c08f9eafb72f1559315ef8
FrhnSpwli <96256195+FrhnSpwli@users.noreply.github.com>
2023-11-23 07:20:00 +0800

    add order layout

---
+++ feature/order/OrderLayout.tsx
@@ -0,0 +1,12 @@
+"use client";
+
+import { OrderContainer } from "./styles";
+import Tabs from "./Tabs";
+
+export const OrderLayout = () => {
+  return (
+    <OrderContainer>
+      <Tabs />
+    </OrderContainer>
+  );
+};
