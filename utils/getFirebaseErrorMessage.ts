331656a32afb0632b0f00e10d682f51705baac03
ahmadsultani <ahmadsultanidayanullah@gmail.com>
2023-11-25 14:34:47 +0800

    feat: error messages

---
+++ utils/getFirebaseErrorMessage.ts
@@ -0,0 +1,16 @@
+import * as ERROR_MESSAGES from "@/constants/errors";
+
+export const getFirebaseErrorMessage = (code: string) => {
+  switch (code) {
+    case "auth/user-not-found":
+      return ERROR_MESSAGES.USER_NOT_FOUND;
+    case "auth/email-already-in-use":
+      return ERROR_MESSAGES.EMAIL_ALREADY_EXIST;
+    case "auth/internal-error":
+      return ERROR_MESSAGES.INTERNAL_ERROR;
+    case "auth/invalid-login-credentials":
+      return ERROR_MESSAGES.INVALID_CREDENTIAL;
+    default:
+      return ERROR_MESSAGES.DEFAULT_ERROR;
+  }
+};
