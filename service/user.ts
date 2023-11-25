b9c77e7435bfbc34041744c263852b953c5225cf
ahmadsultani <ahmadsultanidayanullah@gmail.com>
2023-11-25 14:38:23 +0800

    feat: get current user

---
+++ service/user.ts
@@ -0,0 +1,21 @@
+import { doc, getDoc } from "firebase/firestore";
+
+import { auth, db } from "@/config/firebase";
+
+import { USER_NOT_FOUND } from "@/constants/errors";
+import { IUser } from "@/interfaces/user";
+
+export const getCurrentUser = async () => {
+  const user = auth.currentUser;
+
+  if (!user) throw new Error("User not found!");
+
+  const docRef = doc(db, "user", user.uid);
+  const docSnap = await getDoc(docRef);
+
+  if (!docSnap.exists()) {
+    throw new Error(USER_NOT_FOUND);
+  }
+
+  return docSnap.data() as IUser;
+};
