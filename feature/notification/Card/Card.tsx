7aa6330f7e9f16c8f5d43afe1b35fa6e1bed3e11
FrhnSpwli <andifarhansappewali407@gmail.com>
2023-12-03 16:56:07 +0800

    feat: notification

---
+++ feature/notification/Card/Card.tsx
@@ -0,0 +1,52 @@
+import { useState } from "react";
+
+import { Modal } from "@/components/Modal";
+
+import * as NotificationCard from "../styles";
+import { Typography } from "@mui/material";
+
+interface CardProps {
+  id: number;
+  title: string;
+  description: string;
+  time: string;
+  read: boolean;
+  onMarkAsRead: (id: number) => void;
+}
+
+export const Card: React.FC<CardProps> = ({
+  title,
+  description,
+  time,
+  read,
+}) => {
+  const [isPopupVisible, setIsPopupVisible] = useState(false);
+
+  return (
+    <NotificationCard.Item
+      isRead={read}
+      onClick={() => setIsPopupVisible(true)}
+    >
+      <NotificationCard.Content>
+        <NotificationCard.ItemDate>{time}</NotificationCard.ItemDate>
+        <NotificationCard.ItemTitle>{title}</NotificationCard.ItemTitle>
+        <NotificationCard.ItemDescription>
+          {description}
+        </NotificationCard.ItemDescription>
+      </NotificationCard.Content>
+      <NotificationCard.Circle isRead={read}></NotificationCard.Circle>
+
+      {isPopupVisible && (
+        <Modal
+          isOpen={isPopupVisible}
+          title={title}
+          type="ok"
+          onComplete={() => setIsPopupVisible(false)}
+          onClose={() => setIsPopupVisible(false)}
+        >
+          <Typography fontSize="12px">{description}</Typography>
+        </Modal>
+      )}
+    </NotificationCard.Item>
+  );
+};
